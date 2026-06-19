2a8384df07b302aa29756e1c64c47939b85b9e86
Wang Xin <xinwang.art@gmail.com>
2026-06-19 17:41:45 +0800

    Delete src directory

--- src/hooks/hook-realesrgan.py
+++
@@ -1,6 +0,0 @@
-# hook-realesrgan.py
-# Place this file in your project root (same dir as main.py).
-
-from PyInstaller.utils.hooks import collect_all
-
-datas, binaries, hiddenimports = collect_all("realesrgan")
